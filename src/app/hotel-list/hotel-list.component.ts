5729781f864f12ee611de7ce768ede076089d99c
ismaila10 <lodia.ismaila@gmail.com>
2022-03-29 02:25:32 +0200

    filtre ok

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -21,29 +21,46 @@ export class HotelListComponent implements OnInit {
     },
     {
       hotelId: 2,
-      hotelName: "swiss",
+      hotelName: "callback",
       description: "Top ok cool trankil nice génial, magnifique",
       price: 120,
       imageUrl: ''
     },
     {
       hotelId: 3,
-      hotelName: "swiss",
+      hotelName: "amen",
       description: "Top ok cool trankil nice génial, magnifique",
       price: 120,
       imageUrl: ''
     }
   ];
   showBadge: boolean = false;
-  hotelFilter = 'mot';
+  private _hotelFilter = 'mot';
   filteredHotels: IHotel[] = [];
 
   ngOnInit(): void {
     this.filteredHotels = this.hotels;
+    this.hotelFilter = "";
   }
 
   toggleIsNewBadge(): void {
     this.showBadge = !this.showBadge;
+  }
+
+  public get hotelFilter(): string {
+    return this._hotelFilter;
+  }
+  public set hotelFilter(filter: string) {
+    this._hotelFilter = filter;
+
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter) : this.hotels;
+  }
+
+  private filterHotels(criteria: string): IHotel[] {
+    criteria = criteria.toLocaleLowerCase();
+
+    const response = this.hotels.filter(x => x.hotelName.toLocaleLowerCase().indexOf(criteria) != -1);
+    return response;
   }
 
 }
